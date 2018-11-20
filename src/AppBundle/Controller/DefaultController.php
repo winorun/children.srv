ae968cedeb881a8dece8d1fd188cc71a4a77bc62
winorun <winorun@mail.ru>
2018-11-20 15:44:47 +0700

    bugfix главная страница

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -20,7 +20,7 @@ class DefaultController extends Controller
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
-       return $this->render('base.html.twig');
+       return $this->render('main.html.twig');
     }
 
     /**
